05aad2d1713ecc65fc73620f20a736f226a34075
mbejan325 <85900925+mbejan325@users.noreply.github.com>
2021-06-23 18:01:50 -0700

    Add files via upload

---
+++ Fast.E36.py
@@ -0,0 +1,13 @@
+E36_owners = ["Sean", "Michael", "Brent", "Zak", "Ethan", "Kyle"]
+number_of_e36_owners = len(E36_owners)
+print(f"There are {number_of_e36_owners} owners in Almaden")
+
+fastest_e36 = E36_owners[1]
+print(f"The fastest E36 is {fastest_e36}")
+
+E36_owners = ["Sean", "Michael", "Brent", "Zak", "Ethan", "Kyle"]
+print(f"The first e36 owner was {E36_owners[2]}")
+
+print(f"{E36_owners[0]} tracks his e36 the most")
+
+print(f"{E36_owners[0:6]} have e36s in Almaden")
